cc834757de0b59c66f6e0f35d462d41366f10484
jayzeng <jay@winsportss.com>
2026-04-16 10:57:01 +0800

    fix: exclude payment/return from CSRF in Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            '/webhook/payment',
+            '/quiz/*/payment/return',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
